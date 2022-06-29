b1a6f2233060540fec20f6c697d61d41411c0708
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-06-29 14:16:32 +0200

    basic setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <h1>Home page</h1>
+  </div>
+);
+
+export default Index;
