3760efbc5bba8fca30d39962446990883734aa44
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-06-30 16:16:43 +0200

    add tailwind styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,18 @@
 import Link from "next/link";
 
 const Navbar = () => (
-  <div>
+  <nav className="flex justify-center space-x-4">
     <Link href="/">
-      <a>Home Page</a>
+      <a className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+        Home Page
+      </a>
     </Link>
     <Link href="/about">
-      <a>About Page</a>
+      <a className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+        About Page
+      </a>
     </Link>
-  </div>
+  </nav>
 );
 
 export default Navbar;
